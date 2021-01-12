b0a8d8ae7e17cf925e119a2438a2fe13cda9a793
梦白 <2122446868@qq.com>
2021-01-12 22:50:04 +0800

    重写父类configure方法 修改默认规则 放行静态资源和登录页，设置访问未授权资源跳转到登录页

--- src/main/java/com/atguigu/security/config/WebAppSecurityConfig.java
+++ src/main/java/com/atguigu/security/config/WebAppSecurityConfig.java
@@ -1,19 +1,59 @@
 package com.atguigu.security.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * WebAppSecurityConfig
- * <功能详细描述>
+ * 配置类
  *
  * @author 赵长春
  * @version [版本号, 2021/1/12 17:16]
  * @see [相关类/方法]
  * @since [产品/模块版本]
  */
-@Configuration
+@Configuration      //替代xml配置文件
 @EnableWebSecurity // 表示启用web安全功能
-public class WebAppSecurityConfig {
+public class WebAppSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    /***
+     * 重写configure
+     * 修改默认规则
+     * 默认规则是访问任何请求都必须要登录
+     * @param security
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity security) throws Exception {
+        //super.configure(security); 注释掉将取消父类方法中的默认规则
+        security
+                //对请求进行授权
+                .authorizeRequests()
+                //针对/index.jsp进行授权
+                .antMatchers("/index.jsp")
+                //可以无条件访问
+                .permitAll()
+                //针对/layui/**进行授权
+                .antMatchers("/layui/**")
+                //可以无条件访问
+                .permitAll()
+                .and()
+                //对请求进行授权
+                .authorizeRequests()
+                // 任意请求
+                .anyRequest()
+                // 需要登录后才可以正常访问
+                .authenticated()
+                .and()
+                // 设置未授权请求跳转到登录页面 默认跳转到表单登录页
+                .formLogin()
+                //跳转到登录页面
+                .loginPage("/index.jsp")
+                // 指定提交登录表单的地址  loginProcessingUrl()方法指定了登录地址，就会覆盖 loginPage()方法中设置的默认值
+                .failureForwardUrl("/to/login");
+
+
+    }
 }
