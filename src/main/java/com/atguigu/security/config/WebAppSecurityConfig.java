f2ec0be3dcee4939794429a94165ef85b1cbf110
梦白 <2122446868@qq.com>
2021-01-12 17:36:36 +0800

    项目搭建

---
+++ src/main/java/com/atguigu/security/config/WebAppSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.atguigu.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+/**
+ * WebAppSecurityConfig
+ * <功能详细描述>
+ *
+ * @author 赵长春
+ * @version [版本号, 2021/1/12 17:16]
+ * @see [相关类/方法]
+ * @since [产品/模块版本]
+ */
+@Configuration
+@EnableWebSecurity // 表示启用web安全功能
+public class WebAppSecurityConfig {
+
+}
